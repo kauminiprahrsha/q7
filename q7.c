80f6dfc3cc8754f5690778fedfc1c859d17de0ce
kauminiprahrsha <148688303+kauminiprahrsha@users.noreply.github.com>
2023-12-17 23:42:22 +0530

    Add files via upload

---
+++ q7.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int x;
+int y;
+
+printf("Enter the x value");
+scanf("%d",&x);
+
+if (x<0)
+        {
+            printf("y=1");
+        }
+ else if (x=0)
+ {
+     printf("y=0");
+ }
+ else
+ {
+     printf("y=-1");
+ }
+
+    return 0;
+}
